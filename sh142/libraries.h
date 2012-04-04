8ce7e14af378742023c0c7f76451d22041dd752e
Venakata Saidatta Munnangi <venkatamunnangi@gmail.com>
2012-04-03 17:57:56 -0700

    adding libraries we might use.

---
+++ sh142/libraries.h
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdlib.h> /* exit func */
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h> // printf, fprintf, popen, FILE and other IO operations.
+#include <unistd.h> /* fork and other miscilleneous*/
+#include <ctype.h> /* isdigit, isalpha and other type funcs*/
+#include <curses.h> /* booleans true - false*/
+#include <fcntl.h>
+#include <errno.h> /* errno */
+#include <pthread.h>
+#include <termios.h>
+#include <termios.h>
+#include <signal.h>
+
+
